79710affc65b992750376dbf17707fc2b6214568
dhananjayaaps <dhananjayaaps@gmail.com>
2024-04-20 17:58:15 +0530

    File uploading created

--- src/main/java/com/example/FitDoc/Application.java
+++ src/main/java/com/example/FitDoc/Application.java
@@ -1,13 +1,25 @@
-package com.example.FitDoc;
+package com.example.FitDoc;//package com.example.FitDoc;
 
+import jakarta.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.FitDoc.service.FilesStorageService;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+	@Resource
+	FilesStorageService storageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Override
+	public void run(String... arg) throws Exception {
+    	storageService.deleteAll();
+		storageService.init();
+	}
 }
